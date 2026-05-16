1ea28910ce8eb2b38f4afdd25ca3a23eee4e2d5f
Alex Schwantes <2829978+alexschwantes@users.noreply.github.com>
2026-05-16 22:15:54 +1000

    feat: add .gitattributes for shell script line endings fix:  Dockerfile permissions fix: nginx ingress connection

--- dockhand/rootfs/etc/cont-init.d/02-data-dir.sh
+++ dockhand/rootfs/etc/cont-init.d/02-data-dir.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Ensure /data/db exists and is owned by the dockhand user
+# Ensure /data/db exists
 # ==============================================================================
 bashio::log.info "Initialising data directory..."
 
 mkdir -p /data/db
-if ! chown -R dockhand:dockhand /data; then
-	bashio::log.warning "Unable to chown /data to dockhand:dockhand (likely restricted mount permissions); continuing"
-fi
